f1b183d0b49869ffe6958f3d4f8c2f79e4dd7cef
Deepak <djamwal10@gmail.com>
2023-02-14 23:57:24 +0530

    ui created using PageView.builder

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/view/widget/news_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: PageView.builder(
+        controller: PageController(initialPage: 0),
+        scrollDirection: Axis.vertical,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return NewsContainer(
+              imgUrl:
+                  "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
+              newsHead: "5 G in india 5G services on October 1",
+              newsDesc:
+                  "Reliance Jio and Airtel are spreading their 5G connectivity across India at a rapid pace. Since the launch of 5G services on October 1, the telecom operators have expanded their 5G coverage in 50 Indian cities (till December 7)and are reaching out to more cities almost every day.",
+              newsUrl:
+                  "https://www.indiatoday.in/technology/news/story/5g-service-now-available-in-50-indian-cities-and-towns-full-list-of-cities-check-if-your-is-listed-2307071-2022-12-09");
+        },
+      ),
     );
   }
 }
---
+++ lib/view/widget/news_container.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class NewsContainer extends StatelessWidget {
+  NewsContainer({
+    super.key,
+    required this.imgUrl,
+    required this.newsHead,
+    required this.newsDesc,
+    required this.newsUrl,
+  });
+
+  String imgUrl;
+  String newsHead;
+  String newsDesc;
+  String newsUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Image.network(
+            imgUrl,
+            height: 400,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  Text(
+                    newsHead,
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    newsDesc,
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print("GOING TO $newsUrl");
+                  },
+                  child: const Text("Read More"),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
